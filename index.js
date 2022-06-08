0759d2aa0a9a5d504facd2fdb05d802838a0567b
leah karienye <karienyeleah@gmail.com>
2022-06-08 16:36:35 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function receivesAFunction (callback) {
+    console.log(callback());
+  }
+  function returnsANamedFunction(name) {
+    console.log(name("thing"));
+  }
+  function returnsAnAnonymousFunction (name) {
+    console.log(name());
+  }
+  /*receivesAFunction ({ fName: 'Annah', lName: 'Kananu' });
+
+  function receivesAFunction (Spy) {
+    return Spy;
+  }
+  
+  receivesAFunction ({ fName: 'Annah', lName: 'Kananu' });
+  function receivesAFunction (Spy) {
+    return Spy;
+  }
+  
+  receivesAFunction ({ fName: 'Annah', lName: 'Kananu' });
+
+
+/*function receivesAFunction (type, returnsANamedFunction) {
+    return rreturnsAnAnonymousFunction(type);
+  }
+  
+  bakingCake('black forest cake', function (type) { return 'Yeey we have, ' + type; });
+  */
